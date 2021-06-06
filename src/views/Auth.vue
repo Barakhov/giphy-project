75173bcc35c2c81163095487bad3c0ffc1b1c889
Alexander Barakhov <a.barakhov@payvision.com>
2021-06-06 19:44:22 +0200

    auth login register

---
+++ src/views/Auth.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="auth-links d--f jc--sa">
+      <router-link to="/auth/login">Iniciar sesión</router-link>
+      <router-link to="/auth/register">Regístrate</router-link>
+    </div>
+
+    <!-- router-view inside a router-view -->
+    <router-view />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/scss/variables.scss';
+
+.auth-links {
+  margin-bottom: 20px;
+
+  a {
+    font-weight: $bold;
+    font-size: $fs-h4;
+    color: $clr-1;
+  }
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="d--f ai--c jc--c clr-1">Register view</h1>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+}
+</script>
+
+<style></style>
